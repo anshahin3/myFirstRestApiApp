9bfaa2dca17604c827d8e36fa90972f2ab704cf8
Abu Nesar <anshahin3@gmail.com>
2020-11-03 10:02:45 +0600

    Added PostgreSQL environment variable and psycopg2

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from db import db
 
 db.init_app(app)
 
-@app.before_first_request                           # create database tables before first request
+@app.before_first_request
 def create_tables():
     db.create_all()
+    
